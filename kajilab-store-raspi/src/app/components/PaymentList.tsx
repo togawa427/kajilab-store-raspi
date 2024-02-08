5972e530009b03fd57c6373591e85e17017367b0
togawa427 <95838999+togawa427@users.noreply.github.com>
2024-02-08 17:06:44 +0900

    feat: TailwindcssとMantineが併用可能に

--- kajilab-store-raspi/src/app/components/PaymentList.tsx
+++ kajilab-store-raspi/src/app/components/PaymentList.tsx
@@ -10,11 +10,12 @@ type PaymentListProps = {
 const PaymentList = ({payments}:PaymentListProps) => {
   return (
     <div>
-        <Table className="w-full text-3xl bg-yellow-100 shadow rounded #__next">
+        <Table className="w-full text-3xl bg-yellow-100 shadow rounded">
         <Table.Tbody>
             {payments.map((payment) => (
-                <Table.Tr className="border-2 border-amber-300" key={payment.id}>
-                    <Table.Td className="py-2 px-4">{new Date(payment.pay_at).getMonth()+1}/{new Date(payment.pay_at).getDate()}</Table.Td>
+                // <Table.Tr className="border-2 border-amber-300" key={payment.id}>
+                <Table.Tr key={payment.id}>
+                    <Table.Td className="">{new Date(payment.pay_at).getMonth()+1}/{new Date(payment.pay_at).getDate()}</Table.Td>
                     <Table.Td>{new Date(payment.pay_at).getHours().toString().padStart(2, '0')}:{new Date(payment.pay_at).getMinutes().toString().padStart(2, '0')}</Table.Td>
                     <Table.Td>{payment.products[0] ? payment.products[0].name:''}{payment.products[1] ? ' など':''}</Table.Td>
                     <Table.Td>{payment.price}円</Table.Td>
@@ -23,11 +24,6 @@ const PaymentList = ({payments}:PaymentListProps) => {
             ))}
         </Table.Tbody>
         </Table>
-        <Select
-            label="Your favorite library"
-            placeholder="Pick vvvalue"
-            data={['React', 'Angular', 'Vue', 'Svelte']}
-        />
     </div>
   )
 }
