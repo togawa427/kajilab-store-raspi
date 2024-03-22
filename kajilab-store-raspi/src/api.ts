06ff948df0a6bf1bd6a1ecac3fb4193e7a71da07
togawa427 <95838999+togawa427@users.noreply.github.com>
2024-03-22 23:24:21 +0900

    feat: 仮データで購入処理できた

--- kajilab-store-raspi/src/api.ts
+++ kajilab-store-raspi/src/api.ts
@@ -1,4 +1,16 @@
+import { BuyProduct } from "./app/features/payment/type";
 import { Payment, Product } from "./types/json";
+
+type CreatePaymentType = {
+    pay_at: string;
+    method: string,
+    user_number: string,
+    products: {
+        id: number;
+        quantity: number;
+        unit_price: number,
+    }[],
+}
 
 export const getPayments = async (): Promise<Payment[]> => {
     const res = await fetch("http://localhost:8080/api/v1/products/buy/logs?limit=5", {cache: "no-store"})  // SSR
@@ -9,7 +21,8 @@ export const getPayments = async (): Promise<Payment[]> => {
 }
 
 export const getProductByBarcode = async (barcode: number): Promise<Product> => {
-    const res = await fetch(`http://localhost:8080/api/v1/products/134912341234`, {cache: "no-store"})
+    const res = await fetch(`http://localhost:8080/api/v1/products/134912341232`, {cache: "no-store"})
+    //const res = await fetch(`http://localhost:8080/api/v1/products/${barcode}`, {cache: "no-store"})
     console.log(res)
 
     const product = await res.json()
@@ -26,4 +39,28 @@ export const deletePayment = async (id: number) => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
     return
+}
+
+export const createPayment = async (buyProducts: BuyProduct[], method: string) => {
+    const currentDatetime = new Date().toISOString();
+    const requestPayment:CreatePaymentType = {
+        pay_at: currentDatetime,
+        method: method,
+        user_number: "",
+        products:[
+            {
+                id: 3,
+                quantity: 2,
+                unit_price: 120,
+            }
+        ]
+    }
+
+    const res = await fetch(`http://localhost:8080/api/v1/products/buy`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestPayment)
+    });
 }
