71e4d36c865e44cf425de0c7d4d6d6df8234afdb
togawa427 <95838999+togawa427@users.noreply.github.com>
2025-01-28 16:32:22 +0900

    style: 梶研Payチャージするボタンであることをわかりやすくした

--- kajilab-store-raspi/src/app/components/RootPage.tsx
+++ kajilab-store-raspi/src/app/components/RootPage.tsx
@@ -31,7 +31,7 @@ const RootPage = ({payments}:RootPageProps) => {
       </div>
       <div className="absolute bottom-5 right-14">
         <Link href={"/kajilabpay"}>
-          <Button size='xl' fullWidth variant="filled" color="orange">梶研Pay関連</Button>
+          <Button size='xl' fullWidth variant="filled" color="orange">梶研Payチャージ</Button>
         </Link>
       </div>
       {/* <div className="text-right">
